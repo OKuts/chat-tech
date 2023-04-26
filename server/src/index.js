31a0b04b302bf675edfc32f5106757ab61b06d98
Ol_Kuts <kuts.ol.vas@gmail.com>
2023-04-26 06:12:29 +0300

    finish

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+
 const app = express()
 import * as env from 'dotenv'
 import {applyRoutes} from './rest/index.js'
 import {createServer} from 'http'
 import {Server} from 'socket.io'
+
 env.config()
 
 const httpServer = createServer(app)
@@ -15,40 +17,35 @@ const io = new Server(httpServer, {
 })
 
 const users = []
-const typingUsers = {}
 io.on('connection', socket => {
     io.emit('moveUser', users)
 
-    socket.on('message', (data)=> {
+    socket.on('message', (data) => {
         io.emit('response', data)
     })
 
-    socket.on('leaveUser', (data)=> {
+    socket.on('leaveUser', (data) => {
         users.splice(users.findIndex(el => el.user === data), 1)
         io.emit('moveUser', users)
     })
 
-    socket.on('newUser', (data)=> {
+    socket.on('newUser', (data) => {
         users.push(data)
         io.emit('moveUser', users)
     })
 
-    socket.on('typing', (data)=> {
-        if (!typingUsers[data.socketID]) {
-            typingUsers[data.socketID] = data
-            io.emit('write', Object.values(typingUsers))
-        }
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('write', data.name)
     })
 
-    socket.on('disconnect', ()=> {
+    socket.on('disconnect', () => {
         users.splice(users.findIndex(el => el.socketID === socket.id), 1)
         io.emit('moveUser', users)
     })
 })
 
-
 applyRoutes(app)
 
-httpServer.listen(process.env.PORT,  () => {
+httpServer.listen(process.env.PORT, () => {
     console.log(`Server started on port: ${process.env.PORT}`)
 })
